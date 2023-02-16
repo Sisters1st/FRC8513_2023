13777b78eb72a5517ccdc9bfe7a7c23edc72e27a
robotics@villawalsh.org <robotics@villawalsh.org>
2023-02-15 19:22:09 -0500

    added basic limelight code set default to flash on

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,9 +10,11 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
-import java.net.http.HttpClient.Redirect;
-import java.util.Set;
 
 import com.revrobotics.CANSparkMax;
 
@@ -121,6 +123,24 @@ public class Robot extends TimedRobot {
 
     SmartDashboard.putNumber ("leftDriveSpeed", leftSpeed);
     SmartDashboard.putNumber("rightDriveSpeed", rightSpeed);
+
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
+    NetworkTableEntry ledMode = table.getEntry("ledMode");
+
+    //read values periodically
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    ledMode.setDouble(3);
+    double area = ta.getDouble(0.0);
+
+    //post to smart dashboard periodically
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
 
   }
 
