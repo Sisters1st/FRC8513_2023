c176ab93fc554c963d2e2631bc4400eb8fc89712
Owen Busler <Owbusler@amazon.com>
2023-03-10 11:02:49 -0800

    reset I when arm comes over

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -61,6 +61,7 @@ public class Robot extends TimedRobot {
   double autoWaitTime;
   int autoStep = 0;
   int autoTholdCount = 0;
+  boolean firstTimeOver = false;
 
   public PIDController turnPID = new PIDController(Setting.turnPID_p, Setting.turnPID_i, Setting.turnPID_d);
   public PIDController drivePID = new PIDController(Setting.drivePID_p, Setting.drivePID_i, Setting.drivePID_d);
